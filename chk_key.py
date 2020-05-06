eb063f413ea26bff24e1b046688a5f62c00bbd01
ARIF <lone_wolf@ASUS.localdomain>
2020-05-06 08:44:23 +0600

    check for a key if it exists in a dictionary

---
+++ chk_key.py
@@ -0,0 +1,13 @@
+dict = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+x=5
+
+if x in dict:
+
+    print('Key is present in the dictionary')
+
+else:
+
+    print('Key is not present in the dictionary')
+
+
